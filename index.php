0bf163d796589a7869941aaaa3b7e9fc71da4eaf
Rodrigo Teixeira <drigoteixeira@gmail.com>
2013-06-26 21:40:37 -0300

    Criação da homepage only-text para o novo portfólio

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+<title>Drigo Teixeira - UI Designer | Entusiasta de Arquitetura de Informação</title>
+
+<!-- METAS -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width">
+<meta name="description" content="Drigo Teixeira é UI Designer e Entusiasta de Arquitetura de Informação. São Paulo, Brasil."/>
+
+<link rel="canonical" href="http://www.drigoteixeira.com/" />
+<link rel="author" href="https://plus.google.com/110545419521358863582"/>
+<link rel="publisher" href="https://plus.google.com/110545419521358863582/"/>
+<meta property='og:locale' content='pt_BR'/>
+<meta property='og:type' content='website'/>
+<meta property='og:title' content='Drigo Teixeira - UI Designer | Entusiasta de Arquitetura de Informação'/>
+<meta property='og:description' content='Drigo Teixeira - UI Designer e Entusiasta de AI'/>
+<meta property='og:url' content='http://www.drigoteixeira.com/'/>
+<meta property='og:site_name' content='Drigo Teixeira - UI Designer | Entusiasta de Arquitetura de Informação'/>
+<meta property='article:publisher' content='http://www.facebook.com/drigoteixeira'/>
+<meta property='fb:admins' content='1271620247'/>
+<meta property='og:image' content='http://www.drigoteixeira.com/blog/wp-content/themes/colective/avatar.jpg'/>
+<meta name="google-site-verification" content="yDJgjNuyB1LON1HIq6aXW3Pv2pieL8002dwz9aBiIV8" />
+ 
+<!-- FAVICONS -->
+<link rel="icon" href="favicon.ico">
+
+<!-- CSS -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css">
+
+<!-- RSS -->
+<link rel="alternate" type="application/rss+xml" title="RSS Feed" href="http://feeds.feedburner.com/rodrigoteixeira" />
+
+<!-- SCRIPTS -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
+<!-- RESPOND -->
+<!--[if lt IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+<![endif]-->
+
+    
+</head>
+<body>
+    
+    <div class="page">
+    	
+    	<div class="container-12">
+    		
+
+			<div class="grid w640 content" role="main">
+				<header id="header">
+	    				<div class="row">
+	    					<h1 id="logo">Hi!</h1>
+	    				</div>
+	    		</header>
+				<div class="row">
+					<div class="c12">
+						<section class="posts">
+							<article class="post">
+								<div class="post-content">
+									<p>My name is Rodrigo Teixeira. I'm UI Designer and IA Enthusiast living in São Paulo, Brazil. I work at A2 Comunicação on the Design Team. </p> 
+									<p>I'm also, co-founder of <a href="http://www.alternativacoletiva.com">Alternativa Coletiva</a> and co-author of <a href="http://www.modularhtml.com.br">Modular HTML</a>.</p>
+									<p>Visit my <a href="http://www.drigoteixeira.com/blog/">blog</a> or follow me on <a href="http://www.twitter.com/drigoteixeira">Twitter</a>, <a href="http://www.facebook.com/drigoteixeira">Facebook</a> or <a href="http://br.linkedin.com/in/drigoteixeira">LinkedIn</a>. Get in touch at <a href="mailto:drigoteixeira@gmail.com">drigoteixeira at gmail.com</a>.</p>
+
+								</div>
+							</article>
+						</section>
+					</div>
+				</div>
+			</div>
+
+			<script type="text/javascript">
+
+	          var _gaq = _gaq || [];
+	          _gaq.push(['_setAccount', 'UA-32844537-1']);
+	          _gaq.push(['_trackPageview']);
+
+	          (function() {
+	            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	          })();
+
+        	</script>
+
+	</body>
+</html>
